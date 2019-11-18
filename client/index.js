1b6d502ab8b880a4e0585996f9cc26790566040b
Dmitriy <dimaorizz@gmail.com>
2019-11-18 18:52:09 +0200

    SOme front-end added

--- client/index.js
+++ client/index.js
@@ -1,6 +1,17 @@
 const genCard = (data) => {
     return `
-        <p>${data.text}</p>
+        <div class="post">
+            <h2 class="post-title">${data.title}</h2>
+            <p class="post-text">${data.text}</p>
+            <div class="post-details">
+                <p class="post-date">${data.date.slice(0, 10)}</p>
+                <p class="post-author">Author: ${data.author}</p>
+                <div class="post-likes">
+                        <p class="post-likes-amount">${data.likes} Likes</p>
+                        <input class="post-btn-like" type="button" value="Like">
+                </div>
+            </div>
+        </div>
     `
 };
 
@@ -11,7 +22,7 @@ class PostsApi{
 }
 
 const findPosts = () => {
-    const $output = document.getElementById('container');
+    const $output = document.getElementById('posts');
 
     PostsApi.get().then((res) => {
         for(let i = 0; i < res.length; i++){
@@ -19,3 +30,5 @@ const findPosts = () => {
         }
     });
 };
+
+document.addEventListener('DOMContentLoaded', findPosts());
